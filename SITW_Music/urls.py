0443644b08aa8f85dacbf6a921594e357ff06efc
Alejandro Contreras <acm24@alumnes.udl.cat>
2015-04-04 12:43:02 +0200

    Added login

--- SITW_Music/urls.py
+++ SITW_Music/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'SITW_Music.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^$', mainpage, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^myplaylists/', include('myplaylists.urls', namespace='myplaylists')),
