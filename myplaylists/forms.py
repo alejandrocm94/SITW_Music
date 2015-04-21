5559071ccf1e6ae242c412ea23040a808c0b4a80
Eric Labara <ericlabaraolvera@gmail.com>
2015-04-21 11:50:24 +0200

    Problem with forms solved

--- myplaylists/forms.py
+++ myplaylists/forms.py
@@ -4,15 +4,19 @@ from myplaylists.models import Artist, Release, Song, Playlist
 class ArtistForm(ModelForm):
     class Meta:
         model = Artist
+        fields = "__all__"
 
 class ReleaseForm(ModelForm):
     class Meta:
         model = Release
+        fields = "__all__"
 
 class SongForm(ModelForm):
     class Meta:
         model = Song
+        fields = "__all__"
 
 class PlaylistForm(ModelForm):
     class Meta:
         model = Playlist
+        fields = "__all__"
