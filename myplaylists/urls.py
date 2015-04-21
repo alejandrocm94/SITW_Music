cefcca45dae38649228fe1eef4f048d4bd858b89
Alejandro Contreras Montañez <acm24@alumnes.udl.cat>
2015-04-21 12:41:27 +0200

    Merge branch 'master' of /home/user09/SDK/SITW_Music with conflicts.

--- myplaylists/urls.py
+++ myplaylists/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 from django.utils import timezone
-from django.views.generic import DetailView, ListView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView, UpdateView
 
 from models import Artist, Release, Song, Playlist
 from forms import ArtistForm, SongForm, PlaylistForm
-from views import PlaylistDetail, PlaylistCreate, PlaylistDelete
+from views import PlaylistDetail, PlaylistCreate
 
 urlpatterns = patterns('',
     # List all user playlist: /myplaylist/
@@ -15,26 +15,23 @@ urlpatterns = patterns('',
             template_name='myplaylists/playlist_list.html'),
             name='playlist'),
 
-    # Playlist details, ex.: /myplaylists/1/
-    url(r'^(?P<pk>\d+)/$',
+    # Restaurant details, ex.: /myplaylists/playlist/1/
+    url(r'^playlist/(?P<pk>\d+)/$',
         PlaylistDetail.as_view(),
         name='playlist_detail'),
 
-    # Create a playlist: /myplaylists/create/
-    url(r'^create/$',
+    # Create a restaurant: /myplaylists/playlist/create/
+    url(r'^playlist/create/$',
         PlaylistCreate.as_view(),
         name='playlist_create'),
 
-    # Edit playlist details, ex: /myplaylists/1/edit/
-    url(r'^(?P<pk>\d+)/edit/$',
+    # Edit restaurant details, ex: /myplaylists/playlist/1/edit/
+    url(r'^playlist/(?P<pk>\d+)/edit/$',
         UpdateView.as_view(
             model=Playlist,
             form_class=PlaylistForm,
             template_name='myplaylists/form.html'),
             name='playlist_edit'),
 
-    # Delete playlist, ex: /myplaylists/1/delete/
-    url(r'^(?P<pk>\d+)/delete/$',
-        PlaylistDelete.as_view(),
-        name='playlist_delete'),
+
     )
